8242e0a2b356df07368bfb8b63bef73ce067a6e3
ant work mac <hoj.jun@gmail.com>
2019-12-09 15:30:08 -0800

    keyword tweeter

---
+++ keywordbot.js
@@ -0,0 +1,30 @@
+// require library
+const twit = require('twit')
+const config = require('./config.js')
+
+
+// link library and config: 1 letter
+const T = new twit(config)
+
+// paramters
+const words = {
+    q: 'Rainbow',
+    count: 5
+}
+
+// function code.. get name, created_at, text
+const getData = (err, data, response) => {
+    const tweet = data.statuses; 
+    for(i=0; i<tweet.length; i++){
+        console.log(`ID: ${tweet[i].id}`);
+        console.log(`Date: ${tweet[i].created_at}`);
+        console.log(`Tweet: ${tweet[i].text}`);
+        console.log(`Retweets: ${tweet[i].retweet_count}`);
+    }
+}
+
+// get id and text 
+
+
+// call 
+T.get('search/tweets', words, getData)
