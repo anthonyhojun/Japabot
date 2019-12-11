268b2180e68cc4c791c7dd1fd379c2413d5a8ab2
ant work mac <hoj.jun@gmail.com>
2019-12-10 16:02:39 -0800

    includes slack integration

--- japabot.js
+++ japabot.js
@@ -1,10 +1,10 @@
 // require library
-const twit = require('twit')
-const config = require('./config.js')
-
+const twit = require('twit');
+const config = require('./config.js');
+const request = require('request');
 
 // link library and config: 1 letter
-const T = new twit(config)
+const T = new twit(config);
 
 // paramters
 const tweeter = {
@@ -14,13 +14,34 @@ const tweeter = {
 
 // function code.. get name, created_at, text
 const getData = (err, data, response) => {
-    console.log(data[0].user.name); 
-    console.log(data[0].created_at);
-    console.log(data[0].text);
-}
+//    console.log(data[0].user.name); 
+//    console.log(data[0].created_at);
+//    console.log(data[0].text);
+//        console.log('err!', err);
 
-// test comment2
+    const options = {
+        uri: 'https://hooks.slack.com/services/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',
+        method: 'POST',
+        json: {
+          "text": data[0].user.name + ' ' + data[0].text
+        }
+      };
+      
+      request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+        // console.log(body) // Print the shortened url.
+        //  console.log('starting')
+        //  console.log(response)
+        } else {
+            console.log('error!', error)
+        }
+      });
+      
+}
 
 
 // call 
 T.get('statuses/user_timeline', tweeter, getData)
+
+
+
